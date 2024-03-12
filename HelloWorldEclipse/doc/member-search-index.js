29c074510f83bc569afec161cd2e8b165a89fa0f
Kabir Kashif <kkas0007@student.monash.edu>
2024-03-12 10:07:46 +0500

    fixing files

--- HelloWorldEclipse/doc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"com.itbulls","c":"HelloWorld","l":"HelloWorld()","u":"%3Cinit%3E()"},{"p":"com.itbulls","c":"HelloWorld","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
--- HelloWorldEclipse/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"HelloWorldEclipse","l":"com.itbulls"}];updateSearchResults();
